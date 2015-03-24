1e6fe643237250eb797c7383ce0040ae868f7813
phax <philip@helger.com>
2015-03-24 17:24:15 +0100

    Extracted AbstractHCLabel

--- src/main/java/com/helger/html/hc/html/HCLabel.java
+++ src/main/java/com/helger/html/hc/html/HCLabel.java
@@ -19,108 +19,18 @@ package com.helger.html.hc.html;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import com.helger.commons.microdom.IMicroElement;
-import com.helger.commons.string.StringHelper;
-import com.helger.commons.string.ToStringGenerator;
-import com.helger.html.CHTMLAttributes;
-import com.helger.html.EHTMLElement;
-import com.helger.html.hc.IHCElement;
 import com.helger.html.hc.IHCNode;
 import com.helger.html.hc.IHCNodeBuilder;
-import com.helger.html.hc.conversion.IHCConversionSettingsToNode;
-import com.helger.html.hc.impl.AbstractHCElementWithChildren;
 
 /**
  * Represents an HTML &lt;label&gt; element
  *
  * @author Philip Helger
  */
-public class HCLabel extends AbstractHCElementWithChildren <HCLabel>
+public class HCLabel extends AbstractHCLabel <HCLabel>
 {
-  private String m_sFor;
-  private String m_sForm;
-
   public HCLabel ()
-  {
-    super (EHTMLElement.LABEL);
-  }
-
-  @Nullable
-  public String getFor ()
-  {
-    return m_sFor;
-  }
-
-  /**
-   * Indicates that this label is used as the description for another object.
-   *
-   * @param sFor
-   *        The HTML ID of the other object.
-   * @return this
-   */
-  @Nonnull
-  public HCLabel setFor (@Nullable final String sFor)
-  {
-    m_sFor = sFor;
-    return this;
-  }
-
-  /**
-   * Indicates that this label is used as the description for another object.
-   *
-   * @param aFor
-   *        The HTML of the other object.
-   * @return this
-   */
-  @Nonnull
-  public HCLabel setFor (@Nullable final IHCElement <?> aFor)
-  {
-    if (aFor == null)
-      m_sFor = null;
-    else
-      m_sFor = aFor.ensureID ().getID ();
-    return this;
-  }
-
-  @Nullable
-  public String getForm ()
-  {
-    return m_sForm;
-  }
-
-  /**
-   * The value of the id attribute on the form with which to associate the
-   * element.
-   *
-   * @param sForm
-   *        The HTML ID of the form.
-   * @return this
-   */
-  @Nonnull
-  public HCLabel setForm (@Nullable final String sForm)
-  {
-    m_sForm = sForm;
-    return this;
-  }
-
-  @Override
-  protected void applyProperties (final IMicroElement aElement, final IHCConversionSettingsToNode aConversionSettings)
-  {
-    super.applyProperties (aElement, aConversionSettings);
-    if (StringHelper.hasText (m_sFor))
-      aElement.setAttribute (CHTMLAttributes.FOR, m_sFor);
-    if (StringHelper.hasText (m_sForm))
-      aElement.setAttribute (CHTMLAttributes.FORM, m_sForm);
-  }
-
-  @Override
-  public String toString ()
-  {
-    return ToStringGenerator.getDerived (super.toString ())
-                            .appendIfNotNull ("for", m_sFor)
-                            .appendIfNotNull ("form", m_sForm)
-                            .toString ();
-  }
+  {}
 
   /**
    * Create a new LABEL element with the passed child text
