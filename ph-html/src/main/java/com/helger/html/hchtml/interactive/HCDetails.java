9a850074ae47e8b80545370e92cb19606041cb37
Philip Helger <philip@helger.com>
2015-07-30 16:40:14 +0200

    Moved

--- ph-html/src/main/java/com/helger/html/hchtml/interactive/HCDetails.java
+++ ph-html/src/main/java/com/helger/html/hchtml/interactive/HCDetails.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.html.hchtml.base;
+package com.helger.html.hchtml.interactive;
 
 import javax.annotation.Nonnull;
 
--- ph-html/src/main/java/com/helger/html/hchtml/interactive/HCMenu.java
+++ ph-html/src/main/java/com/helger/html/hchtml/interactive/HCMenu.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.html.hchtml.base;
+package com.helger.html.hchtml.interactive;
 
 import com.helger.html.EHTMLElement;
 import com.helger.html.annotation.DeprecatedInHTML4;
